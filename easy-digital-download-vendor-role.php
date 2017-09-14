2e83e49a6acd01839e4f8e617da4bd4195659aec
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-13 22:00:46 -0500

    added main php file

---
+++ easy-digital-download-vendor-role.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: EDD Vendor Role
+ * Plugin URI: https://github.com/KevinDahlberg/easy-digital-downloads-vendor-role-plugin
+ * Description: Extension of the EDD plugin that changes the Vendor Role to allow vendors to publish posts
+ * Version: 1.0
+ * Author: Kevin Dahlberg
+ * Author URI: http://kevindahlberg.com
+ * License: GPL 3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+register_activation_hook()
+
+register_deactivation_hook()
+
+register_uninstall_hook()
